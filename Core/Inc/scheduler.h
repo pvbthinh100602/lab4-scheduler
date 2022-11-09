0ff27e1253d24edc7f9bcdf52bb05e59ca85279d
Pham Thinh <phamvietbaothinh2002@gmail.com>
2022-11-09 09:27:57 +0700

    Initial code.

---
+++ Core/Inc/scheduler.h
@@ -0,0 +1,30 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: Oct 31, 2022
+ *      Author: phamv
+ */
+
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
+
+#include "main.h"
+
+typedef struct{
+	void (* pTask)(void);
+	uint32_t Delay;
+	uint32_t Period;
+	uint8_t RunMe;
+	uint32_t TaskID;
+} sTask;
+
+#define SCH_MAX_TASKS 40
+#define NO_TASK_ID 0
+
+void SCH_Init(void);
+void SCH_Update(void);
+void SCH_Dispatch_Tasks(void);
+uint32_t SCH_Add_Task(void(*pFunc)(), uint32_t delay, uint32_t period);
+uint8_t SCH_Delete_Task(uint32_t taskID);
+
+#endif /* INC_SCHEDULER_H_ */
---
+++ Core/Src/scheduler.c
@@ -0,0 +1,66 @@
+/*
+ * scheduler.c
+ *
+ *  Created on: Oct 31, 2022
+ *      Author: phamv
+ */
+#include "scheduler.h"
+
+sTask SCH_tasks_G[SCH_MAX_TASKS];
+
+void SCH_Init(void){
+	unsigned char i;
+	for(i = 0; i < SCH_MAX_TASKS; i++){
+		SCH_Delete_Task(i);
+	}
+}
+
+void SCH_Update(void){
+	uint32_t index;
+	for(index = 0; index < SCH_MAX_TASKS; index++){
+		if(SCH_tasks_G[index].pTask){
+			if(SCH_tasks_G[index].Delay > 0){
+				SCH_tasks_G[index].Delay--;
+			} else {
+				SCH_tasks_G[index].RunMe += 1;
+				SCH_tasks_G[index].Delay = SCH_tasks_G[index].Period;
+			}
+		}
+	}
+}
+
+uint32_t SCH_Add_Task(void(*pFunc)(), uint32_t delay, uint32_t period){
+	uint32_t index = 0;
+	while(SCH_tasks_G[index].pTask != 0 && index < SCH_MAX_TASKS) index++;
+	if(index == SCH_MAX_TASKS){
+		return SCH_MAX_TASKS;
+	}
+	SCH_tasks_G[index].pTask = pFunc;
+	SCH_tasks_G[index].Period = period;
+	SCH_tasks_G[index].Delay = delay;
+	SCH_tasks_G[index].RunMe = 0;
+	SCH_tasks_G[index].TaskID = index;
+	return index;
+}
+
+void SCH_Dispatch_Tasks(void){
+	for(int index = 0; index < SCH_MAX_TASKS; index++){
+		if(SCH_tasks_G[index].RunMe > 0){
+			SCH_tasks_G[index].RunMe--;
+			(*SCH_tasks_G[index].pTask)();
+			if(SCH_tasks_G[index].Period == 0) SCH_Delete_Task(index);
+		}
+	}
+}
+
+uint8_t SCH_Delete_Task(uint32_t index){
+	if(SCH_tasks_G[index].pTask == 0){
+		return 0;
+	} else {
+		SCH_tasks_G[index].pTask = 0;
+		SCH_tasks_G[index].Delay = 0;
+		SCH_tasks_G[index].Period = 0;
+		SCH_tasks_G[index].RunMe = 0;
+		return 1;
+	}
+}
